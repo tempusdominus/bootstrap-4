8174f6478108c5cbcb72aa00ffc19cdb107cabbd
Anton Bagdatyev (Tonix) <antonytuft@gmail.com>
2020-05-02 12:57:27 +0200

    Next patch version

--- build/css/tempusdominus-bootstrap.css
+++ build/css/tempusdominus-bootstrap.css
@@ -1,5 +1,5 @@
 /*@preserve
- * Tempus Dominus Bootstrap4 v5.15.0 (https://tempusdominus.github.io/bootstrap-4/)
+ * Tempus Dominus Bootstrap4 v5.15.1 (https://tempusdominus.github.io/bootstrap-4/)
  * Copyright 2016-2020 Jonathan Peterson
  * Licensed under MIT (https://github.com/tempusdominus/bootstrap-3/blob/master/LICENSE)
  */
